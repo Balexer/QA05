0579a13cbf51fdfba9dae8788c99400cbc57cb2b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:31:06 +0300

    Update Task_3.java

--- src/homework_2/task_3/Task_3.java
+++ src/homework_2/task_3/Task_3.java
@@ -2,13 +2,23 @@ package homework_2.task_3;
 
 public class Task_3 {
     public static void main(String[] args) {
-        int ch;
+        int ch; // отсюда убираем
         int sum = 1;
         // sum = 1 потому что я не знаю как еденицу в цикл запихнуть
         for (ch=1;ch<256;) {
             ch = ch*2 ;
             sum = ch + sum;
         }
+        
+        /*
+        int sum = 0;
+        
+        for (int ch=1;ch<256; ch=ch*2) {
+        sum +=ch
+        }
+        
+        думаю так получше будет
+        */
         System.out.println(sum);
     }
 }
