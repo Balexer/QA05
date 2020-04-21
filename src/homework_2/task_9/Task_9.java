b9744dc55ebc84dafbce8f240413c150a1093f30
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:38:37 +0300

    Update Task_9.java

--- src/homework_2/task_9/Task_9.java
+++ src/homework_2/task_9/Task_9.java
@@ -2,6 +2,7 @@ package homework_2.task_9;
 
 public class Task_9 {
     public static void main(String[] args) {
+        // для решения этой задачи тебе нужен цикл и скорее всего еще один цикл внутри.
         System.out.println("   *      (1)");
         System.out.println("  **");
         System.out.println(" ***");
