0c245caf9eeaf70500c53284fb3926571627f876
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:32:57 +0300

    Update Task_5.java

--- src/homework_2/task_5/Task_5.java
+++ src/homework_2/task_5/Task_5.java
@@ -4,7 +4,8 @@ public class Task_5 {
     public static void main(String[] args) {
         int d;
         double s;
-        for (d = 0; d<20; d++) {
+        // 20 в отдельную переменную
+        for (d = 0; d<20; d++) { // for (int d = 0; d<20; d++) {
             s=d*2.54;
             System.out.println(d + " дюймов = " + s + " сантиметров");
         }
