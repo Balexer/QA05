c161b2bc0543a2bccfb6614ace59f055763ff5f7
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:25:10 +0300

    Update Task_1.java

--- src/homework_2/task_1/Task_1.java
+++ src/homework_2/task_1/Task_1.java
@@ -4,6 +4,8 @@ public class Task_1 {
     public static void main(String[] args) {
         int day;
         double km_1 = 10;
+        // магическое число 8 в отдельную переменную, если я верно помню, то кол-во дней = 7,тогда получается след-ее:
+        // for (day = 1; day < 7; day++) {
         for (day = 2; day < 8; day++) {
             km_1 = km_1*1.1 + 10;
         }
