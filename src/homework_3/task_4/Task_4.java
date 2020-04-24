74f3976c5974a2022ba94b3a681aba91eef66d5d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-24 10:21:59 +0300

    Update Task_4.java

--- src/homework_3/task_4/Task_4.java
+++ src/homework_3/task_4/Task_4.java
@@ -5,17 +5,19 @@ public class Task_4 {
     public static void main(String[] args) {
         int[][] array = new int[9][9];
 
+        // заполнение в отдельный метод
         for (int i = 1; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = i * (j + 1);
             }
         }
 
+        // печать в отдельный метод
         for (int i = 1; i < array.length; i++) {
             System.out.println(Arrays.toString(array[i]));
         }
     }
-
+// пустая строка не нужна
 
 }
-//
+// - перед отправкой ДЗ очищай код от такого
