9f3446c7ebb7aa614480bdee56e54064b7a91aa4
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-28 19:41:09 +0300

    Update Task_3.java

--- src/homework_4/task_3/Task_3.java
+++ src/homework_4/task_3/Task_3.java
@@ -11,7 +11,12 @@ public class Task_3 {
         newPerson[1] = new Apple("Maks", 15);
         newPerson[2] = new Apple("Maks", 15);
 
+        // я думаю цель залачи не вывести на экран одинаковые, а посчитать их кол-во
+        // логику по поиску в отдельный метод
+        
+        // 3 -> newPerson.length
         for (int i = 0; i<3; i++){
+            // 3 -> newPerson.length
             for(int j = i + 1; j<3; j++) {
                 System.out.println(newPerson[i].show() + " Cравним с  " + newPerson[j].show() + "equals: " +newPerson[i].equals(newPerson[j]));
 
