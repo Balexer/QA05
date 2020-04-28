fbf2dfe72fea51abdbe0dd7e71bae6a9dee5710d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-28 19:37:21 +0300

    Update Task_2.java

--- src/homework_4/task_2/Task_2.java
+++ src/homework_4/task_2/Task_2.java
@@ -1,5 +1,6 @@
 package homework_4.task_2;
 
+// +, думаю все же имеет смысл переписать пример себе
 public class Task_2 {
     public static void main(String[] args) {
         System.out.println("На экран будет выведено 'constructor value'");
