837ed4d6048104a06009eed6e401fcbf99abf132
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-28 19:35:35 +0300

    Update Task_1.java

--- src/homework_4/task_1/Task_1.java
+++ src/homework_4/task_1/Task_1.java
@@ -2,7 +2,7 @@ package homework_4.task_1;
 
 public class Task_1 {
     public static void main(String[] args) {
-        Apple iam = new Apple();
+        Apple iam = new Apple(); // iam -> apple/redApple
         System.out.println(iam.show());
     }
 }
