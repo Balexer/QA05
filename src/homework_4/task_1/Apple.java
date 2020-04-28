287ba17fdc5a860935861068e0410996d211f7de
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-28 19:34:39 +0300

    Update Apple.java

--- src/homework_4/task_1/Apple.java
+++ src/homework_4/task_1/Apple.java
@@ -5,13 +5,15 @@ import java.util.Objects;
 public class Apple {
    private String name;
    private Integer age;
+   // я вроде вам еще не показывал константы, но правильно будет использовать final + caps lock
+   //private static final String STATIC_BLOCK_MESSAGE = "Hello from static";
    private static String text = "Hello from static";
 
     public Apple(String name, Integer age) {
         this.name = name;
         this.age = age;
-    }
-    public Apple () {
+    } // абзац после констуктора
+    public Apple () {     // констурктор без аргументов должен идти первым
         this ("Alex", 19);
     }
 
@@ -37,8 +39,8 @@ public class Apple {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) return true; // не забывай использовать {} после if
+        if (o == null || getClass() != o.getClass()) return false; // не забывай использовать {} после if
         Apple apple = (Apple) o;
         return Objects.equals(name, apple.name) &&
                 Objects.equals(age, apple.age);
