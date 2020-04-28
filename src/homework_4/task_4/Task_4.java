af6f5a5ad1209e5e0e0f5615ebcb2716e3b99a03
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-28 19:42:57 +0300

    Update Task_4.java

--- src/homework_4/task_4/Task_4.java
+++ src/homework_4/task_4/Task_4.java
@@ -4,9 +4,9 @@ import homework_4.task_1.Apple;
 
 public class Task_4 {
     public static void main(String[] args) {
-        Apple p_1 = new Apple();
-        Apple p_2 = new Apple();
-        Apple p_3 = new Apple();
+        Apple p_1 = new Apple(); // p_1 плохое название переменной
+        Apple p_2 = new Apple(); // p_2 плохое название переменной
+        Apple p_3 = new Apple(); // p_3 плохое название переменной
         System.out.println(p_1.showForText());
         System.out.println(p_2.showForText());
         System.out.println(p_3.showForText()); //проверочка
