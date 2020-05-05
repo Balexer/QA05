24d860b94a6160d3057faf571f7a64503c14c2dd
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-05 21:02:50 +0300

    Update House.java

--- src/homework_5/task_3/House.java
+++ src/homework_5/task_3/House.java
@@ -1,6 +1,7 @@
 package homework_5.task_3;
 
 public class House implements Types {
+    // поля класса должны быть приватными!
     Integer floors;
     Boolean heating;
     Integer residents;
@@ -9,29 +10,30 @@ public class House implements Types {
         this.floors = floors;
         this.residents = residents;
         this.heating = heating;
-
+// пустая строка не нужна
     }
 
+    // лучше переопредели метод toString() он как раз для этих целей
     public String show() {
         return "How many floors  " + this.floors + "; " + "How many residents  " + this.residents + "; " + "Is there any heating  " + this.heating;
     }
 
-
+// пустая строка не нужна
     @Override
     public void floors() {
         System.out.println(this.floors);
-
+// пустая строка не нужна
     }
 
     @Override
     public void heating() {
         System.out.println(this.heating);
-
+// пустая строка не нужна
     }
 
     @Override
     public void residents() {
         System.out.println(this.residents);
-
+// пустая строка не нужна
     }
 }
