c3e832d1a05bb9a010b1c3f5d853178e550046a8
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-09 12:32:11 +0300

    Update Task_1.java

--- src/homework_6/task_1/Task_1.java
+++ src/homework_6/task_1/Task_1.java
@@ -1,11 +1,12 @@
 package homework_6.task_1;
 
+// форматируй код
 public class Task_1 {
     public static void main(String[] args) {
         int[] array = {8, 2, 4, 3, 6};
         getArray(array,0, " ", " ");
-    }
-    public static void getArray(int[] array, int i, String g, String j) {
+    } // здесь абзац
+    public static void getArray(int[] array, int i, String g, String j) { // давй переменным "говорящие" названия
         if (i < array.length) {
             System.out.println(g + array[i]);
             getArray(array,i+1, g + j, j);
