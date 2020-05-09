17aab19eb4f6e57b3347f467ead9844f9001353f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-09 12:44:53 +0300

    Update Transmission.java

--- src/homework_6/task_2/Transmission.java
+++ src/homework_6/task_2/Transmission.java
@@ -9,12 +9,12 @@ public class Transmission {
         this.transmission=0; // поле типа int (примитив) по умолчанию будет 0, твоя строка лишняя
     }
     public void transmissionUp() {
-        if(transmission<=7) {
+        if(transmission<=7) { // 7 в константу
             transmission++;
         }
     }
     public void transmissionDown() {
-        if(transmission>0) {
+        if(transmission>0) { // 0 в константу
             transmission--;
         }
     }
