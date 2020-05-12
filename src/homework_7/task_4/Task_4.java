be0e97ac91be39dcecf0e54f9762fc1539fbc300
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-12 22:24:20 +0300

    Update Task_4.java

--- src/homework_7/task_4/Task_4.java
+++ src/homework_7/task_4/Task_4.java
@@ -34,8 +34,7 @@ public class Task_4 {
         int countWord = 0;
 
         for (int i = 0; i> -1; i++) {
-            i = text.indexOf(Word, i); // я думаю в этой строке может быть проблема. 
-            // Попробуй для отладки использовать текст, в котором ты можешь слова сам посчитать
+            i = text.indexOf(Word, i); 
             if (i == -1) {
                 break;
             }
