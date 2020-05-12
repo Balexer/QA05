209def3410db36845da6456028d11eb714f89768
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-12 22:17:30 +0300

    Update Task_8.java

--- src/homework_7/task_8/Task_8.java
+++ src/homework_7/task_8/Task_8.java
@@ -2,19 +2,21 @@ package homework_7.task_8;
 
 import java.util.Arrays;
 
+// Стоит сделать хранилище, в нем массив Car и у Car сделать поле с двигателем
 public class Task_8 {
     public static void main(String[] args) {
         String[] array = {"1.4", "3.0", "1.4", "2.0", "1.8"};
         System.out.println(Arrays.toString(array));
         for (int i = 0; i < array.length; ++i) {
-            if (array[i] == "1.4") {
+            if (array[i] == "1.4") { // классы сравниваем только через equals()
                 array[i] = "2.0T";
             }
-            if (array[i] == "1.8") {
+            if (array[i] == "1.8") { // классы сравниваем только через equals()
                 array[i] = "1.8T";
             }
         }
         System.out.println(Arrays.toString(array));
         //Пробовал через replace но ничего не работало, поэтому сделал так.
+        
     }
 }
