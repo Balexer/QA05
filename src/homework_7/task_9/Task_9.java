690bd2746834bdd7ad7ef0ba0ee6734c8f7609c7
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-12 22:18:50 +0300

    Update Task_9.java

--- src/homework_7/task_9/Task_9.java
+++ src/homework_7/task_9/Task_9.java
@@ -4,7 +4,7 @@ public class Task_9 {
     public static void main(String[] args) {
         String[] array = {"work again", "war", "gold", "minaaaa"};
         for (int i = 0; i < array.length; ++i) {
-            if (array[i].length() > 5) {
+            if (array[i].length() > 5) { //5 в переменную
                 System.out.println(array[i]);
             }
         }
