01075fdccbcbb1eb20c0b43ea928422936d5501b
Balexer <balexer@mail.ru>
2020-05-14 21:17:48 +0300

    DZ8

--- src/homework_8/task_5/Task_5.java
+++ src/homework_8/task_5/Task_5.java
@@ -2,34 +2,43 @@ package homework_8.task_5;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Task_5 {
     public static void main(String[] args) {
         int quantity = 100000;
-        ArrayList<Integer> integ = new ArrayList<>();
-        for (int i = 0; i <= quantity; i++) {
-            integ.add(0, 1);
-        }
+        ArrayList<Integer> arrayInteger = new ArrayList<>();
+        add(arrayInteger, quantity);
         Long start = System.currentTimeMillis();
-        for (int i = 0; i <= quantity; i++) {
-            int mid = integ.size() / 2;
-            integ.remove(mid);
-        }
-        Long finish = System.currentTimeMillis();
-        System.out.println("Результат проверки ArrayList " + (finish-start));
+        remove(arrayInteger);
 
-        LinkedList<Integer> integ1 = new LinkedList<>();
-        for (int j = 0; j <= quantity; j++) {
-            integ1.add(0, 1);
-        }
+        Long finish = System.currentTimeMillis();
+        System.out.println("Результат проверки ArrayList " + (finish - start));
+
+        LinkedList<Integer> linkedInteger = new LinkedList<>();
+        add(linkedInteger, quantity);
+
         Long start1 = System.currentTimeMillis();
-        for (int j = 0; j <= quantity; j++) {
-            int mid = integ1.size() / 2;
-            integ1.remove(mid);
-        }
+
+        remove(linkedInteger);
+
         Long finish1 = System.currentTimeMillis();
 
         System.out.println("Результат проверки LinkedList " + (finish1 - start1));
 
+    }
+
+    private static void add(List<Integer> list, int count) {
+        for (int i = 0; i <= count; i++) {
+            list.add(1);
+        }
+    }
+
+    private static void remove(List<Integer> list) {
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            int mid = list.size() / 2;
+            list.remove(mid);
+        }
     }
 }
--- src/homework_8/task_6/Task_6.java
+++ src/homework_8/task_6/Task_6.java
@@ -2,33 +2,40 @@ package homework_8.task_6;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Task_6 {
     public static void main(String[] args) {
-        int b = 100000;
+        int count = 90000;
         ArrayList<Integer> arrayIntegers = new ArrayList<>();
-        for (int i = 0; i <= b; i++) {
-            arrayIntegers.add(0, 1);
-        }
+
+        add(arrayIntegers, count);
+
         Long start = System.currentTimeMillis();
-        for (int i = 0; i <= b; i++) {
-            arrayIntegers.remove(0);
-        }
+        remove(arrayIntegers);
         Long finish = System.currentTimeMillis();
-        System.out.println("Результат проверки ArrayList " + (finish-start));
+        System.out.println("Результат проверки ArrayList " + (finish - start));
 
         LinkedList<Integer> linkedIntegers = new LinkedList<>();
-        for (int j = 0; j <= b; j++) {
-            linkedIntegers.add(0, 1);
-        }
+        add(linkedIntegers, count);
+
         Long start1 = System.currentTimeMillis();
-        for (int j = 0; j <= b; j++) {
-            linkedIntegers.remove(0);
-        }
+        remove(linkedIntegers);
+
         Long finish1 = System.currentTimeMillis();
 
         System.out.println("Результат проверки LinkedList " + (finish1 - start1));
+    }
 
+    private static void add(List<Integer> list, int count) {
+        for (int i = 0; i <= count; i++) {
+            list.add(1);
+        }
+    }
 
+    private static void remove(List<Integer> list) {
+        while (list.size() > 0) {
+            list.remove(0);
+        }
     }
 }
