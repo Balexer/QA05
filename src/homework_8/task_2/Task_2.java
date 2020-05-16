d0ece72433fbd1b6a84d06deadc885b968dcc0e3
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-16 13:20:48 +0300

    Update Task_2.java

--- src/homework_8/task_2/Task_2.java
+++ src/homework_8/task_2/Task_2.java
@@ -8,11 +8,13 @@ public class Task_2 {
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();
         int countElem = 100;
+        / заполнение в отдедьный метод
         for (int i = 0; i <= countElem; i++) {
             integers.add(0, 1 + i);
 
         }
         System.out.println(integers);
+        / удаление в отедьный метод
         Iterator<Integer> iterator = integers.iterator();
         while (iterator.hasNext()) {
             Integer next = iterator.next();
@@ -20,7 +22,7 @@ public class Task_2 {
                 iterator.remove();
             }
         }
-
+// абзац думаю здесь не особо нужен
         System.out.println(integers);
     }
 }
