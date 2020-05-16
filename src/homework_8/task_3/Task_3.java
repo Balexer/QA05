2aaef7f6314c11f1b4d0cef7830468cb33d319a1
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-16 13:21:28 +0300

    Update Task_3.java

--- src/homework_8/task_3/Task_3.java
+++ src/homework_8/task_3/Task_3.java
@@ -3,6 +3,7 @@ package homework_8.task_3;
 import java.util.ArrayList;
 import java.util.List;
 
+//+
 public class Task_3 {
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();
@@ -11,12 +12,15 @@ public class Task_3 {
         integers.add(2);
         integers.add(2);
         System.out.println(integers);
+       
         List<Integer> integersNew = new ArrayList<>();
         integersNew.add(1);
         integersNew.add(1);
         integersNew.add(3);
         integersNew.add(3);
+        
         integers.removeAll(integersNew);
+        
         System.out.println(integers);
     }
 }
